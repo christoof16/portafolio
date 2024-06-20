13b1bcb1866d604bd57781e73eaf76e19a65a132
chris <christoof16@gmail.com>
2024-06-20 14:56:55 -0600

    validacion del formulario

--- js/script.js
+++ js/script.js
@@ -7,7 +7,31 @@ let navBarraContacto =  document.querySelector('.nav__barra:last-child');
 let headerBoton = document.querySelector('.header__boton');
 let headerBoton2 = document.querySelector('.header__boton2');
 
+//FORMULARIOS
+const contactoFlex =  document.querySelector('.contacto__flex');
+const contactoName =  document.querySelector('.contacto__name');
+const contactoPhone =  document.querySelector('.contacto__phone');
+const contactoCorreo =  document.querySelector('.contacto__correo');
+const contactoIssue =  document.querySelector('.contacto__issue');
+const contactoDescription =  document.querySelector('.contacto__description');
 
+const contactoBoton = document.querySelector('.contacto__boton ');
+console.log(contactoBoton);
+
+
+const enviarCorreo ={
+    nombre: '',
+    telefono: '',
+    email: '',
+    asunto: '',
+}
+
+
+contactoName.addEventListener('blur',validacionFormulario);
+contactoPhone.addEventListener('blur',validacionFormulario);
+contactoCorreo.addEventListener('blur',validacionFormulario);
+contactoIssue.addEventListener('blur',validacionFormulario);
+// contactoDescription.addEventListener('blur',validacionFormulario);
 
 headerBoton.addEventListener('click',aparecer);
 headerBoton2.addEventListener('click',desaparecer);
@@ -15,6 +39,10 @@ headerBoton2.addEventListener('click',desaparecer);
 
 window.addEventListener('resize',tamanio);
 window.addEventListener('DOMContentLoaded',tamanio);
+
+contactoFlex.addEventListener('submit',enviarDatos);
+
+
 
 const screen ={
     small: 390,
@@ -57,4 +85,95 @@ function desaparecer(){
     headerBoton.style.display="block";
     headerBoton2.style.display="none";
     tamanio();
+}
+
+
+function validacionFormulario(e){
+    const name = e.target.name;
+    if(e.target.value.trim().length < 1){
+        mostrarAlerta(`${name} esta vacio`,e.target.parentElement);
+        enviarCorreo[e.target.nombre] ='';
+        comprobarEmail();
+        return;
+    }
+
+    if(e.target.name === 'email' && !validarEmail(e.target.value)){
+        mostrarAlerta('el correo no es valido',e.target.parentElement);
+        enviarCorreo[e.target.name] ='';
+        comprobarEmail();
+        return;
+    }
+
+    limpiarAlerta(e.target.parentElement);
+
+    enviarCorreo[e.target.name] = e.target.value.trim().toLowerCase();
+
+    comprobarEmail();
+
+}
+
+function mostrarAlerta(mensaje,referencia){
+    // console.log(mensaje);
+
+    limpiarAlerta(referencia);
+
+    const alerta = document.createElement('p');
+    alerta.textContent = mensaje;
+    alerta.classList.add('alerta');
+    contactoFlex.appendChild(alerta);
+
+    referencia.appendChild(alerta);
+
+    setInterval(()=>{
+        alerta.remove();
+    },3000)
+}
+
+function enviarDatos(e){
+    e.preventDefault();
+
+    const exito = document.createElement('p');
+    exito.textContent = 'informacion enviada';
+    exito.classList.add('exito');
+    contactoFlex.appendChild(exito);
+    contactoBoton.style.display ="block";
+
+    setInterval(()=>{
+        contactoBoton.style.display ="none";
+        },0);
+
+    setInterval(()=>{
+    exito.remove();
+    },3000);
+
+    contactoFlex.reset();
+    
+    // console.log('informacion enviada');
+}
+
+function limpiarAlerta(ref){
+    const alerta = ref.querySelector('.alerta');
+
+
+    if(alerta){
+        alerta.remove();
+    }
+
+}
+
+function validarEmail(email){
+    const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ ;
+    const resultado = regex.test(email);
+    return resultado;
+}
+
+function comprobarEmail(){
+    if(Object.values(enviarCorreo).includes('')){
+        contactoBoton.classList.remove('opacity');
+        contactoBoton.disabled = true;
+        return;
+    }
+
+    contactoBoton.classList.add('opacity');
+    contactoBoton.disabled = false;
 }
